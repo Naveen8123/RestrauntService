fcb26f73e3390a61322a0f0e186ef6f607b5d4a1
Naveen <naveennandhi143@gmail.com>
2024-05-30 13:33:36 +0530

    intial commit of Restraunt Service

---
+++ src/main/java/com/_techsoft/microServiceSec/restrauntService/service/RestaurantService.java
@@ -0,0 +1,21 @@
+package com._techsoft.microServiceSec.restrauntService.service;
+
+import com._techsoft.microServiceSec.restrauntService.dao.RestaurantOrderDAO;
+import com._techsoft.microServiceSec.restrauntService.dto.OrderResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestaurantService {
+    @Autowired
+    private RestaurantOrderDAO orderDAO;
+
+    public String greeting() {
+        return "Welcome to Swiggy Restaurant service";
+    }
+
+    public OrderResponseDTO getOrder(String orderId) {
+        return orderDAO.getOrders(orderId);
+    }
+}
+
---
+++ src/test/java/com/_techsoft/microServiceSec/restrauntService/RestrauntServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com._techsoft.microServiceSec.restrauntService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrauntServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
